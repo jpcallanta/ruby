40945409077361bb98a29f30187a28ee0d5ef7ad
jpcallanta <jp@callanta.net>
2015-11-21 14:55:08 -0500

    Rolling back ruby version to 2.1.2, 2.2.2 busted for new mongo gem

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['ruby']['version'] = '2.2.2'
-default['ruby']['source_url'] = 'ftp://ftp.ruby-lang.org/pub/ruby/2.2'
+default['ruby']['version'] = '2.1.2'
+default['ruby']['source_url'] = 'ftp://ftp.ruby-lang.org/pub/ruby/2.1'
 default['ruby']['build_dir'] = '/tmp'
 default['ruby']['install_prefix'] = '/usr/local'
